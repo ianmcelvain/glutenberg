2e1f3ee905e37600ea3bdb890babcc80ac6d2a53
Paul Alexander <paul.alexander@cardinalfinancial.com>
2023-05-18 09:56:46 -0400

    Switch from vite to laravel mix

---
+++ resources/views/sandbox.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet">
+
+{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <style>
+        * {
+            font-family: Nunito Sans, serif;
+        }
+    </style>
+</head>
+<body class="antialiased">
+    <h1 class="text-3xl font-bold underline">Hello</h1>
+</body>
+</html>
