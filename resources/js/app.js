dd5e075f8f04b77a0222a7741b663f73386a73de
Paul Alexander <paul.alexander@cardinalfinancial.com>
2023-05-19 15:43:20 -0400

    Add browser build of isolated block editor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,8 @@
 import './bootstrap';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import BlockEditor from './components/BlockEditor';
+//
+// const blockEditor = new BlockEditor(wp, document.querySelector('#editor'));
+// blockEditor.init();
 
-import IsolatedBlockEditor, {
-    EditorLoaded,
-    DocumentSection,
-    ToolbarSlot,
-    // CollaborativeEditing
-} from '@automattic/isolated-block-editor';
-
-import '@automattic/isolated-block-editor/build-browser/isolated-block-editor.css';
-
-const settings = {
-    //
-}
-
-const root = ReactDOM.createRoot( document.querySelector( '#editor' ) );
-
-root.render(
-    <IsolatedBlockEditor
-        settings={ settings }
-        onSaveContent={ ( html ) => saveContent( html ) }
-        onLoad={ ( parse ) => loadInitialContent( parse ) }
-        onError={ () => document.location.reload() }
-    >
-    </IsolatedBlockEditor>
-);
--- resources/views/sandbox.blade.php
+++ resources/views/sandbox.blade.php
@@ -11,6 +11,13 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400&display=swap" rel="stylesheet">
 
+    <script crossorigin src="https://unpkg.com/react@18/umd/react.production.min.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"></script>
+
+    <script src="{{ asset('js/isolated-block-editor.js') }}"></script>
+    <link rel="stylesheet" href="{{asset('css/core.css')}}">
+    <link rel="stylesheet" href="{{asset('css/isolated-block-editor.css')}}">
+
 {{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <script src="{{ mix('js/app.js') }}" defer></script>
@@ -25,7 +32,11 @@
 <body class="antialiased w-full p-4">
     <h1 class="text-3xl font-bold underline">Hello</h1>
     <section class="flex max-w-3xl mt-4">
-        <textarea id="editor" class="border"></textarea>
+        <textarea id="editor" class="border w-full"></textarea>
     </section>
+
+    <script>
+        wp.attachEditor( document.getElementById( 'editor' ) );
+    </script>
 </body>
 </html>
