cc395c6b9090dad2b29ac73eb82c0708a4c7e081
Ian McElvain <ian.mcelvain@gmail.com>
2023-07-10 22:17:54 -0500

    Add browser and module page

---
+++ public/js/pages/module.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************************!*\
+  !*** ./resources/js/pages/module.js ***!
+  \**************************************/
+// TODO: import modules and write build config to properly serve it
+/******/ })()
+;
---
+++ resources/js/pages/browser.js
@@ -0,0 +1,4 @@
+import BlockEditor from '../blocks/block-editor';
+
+const editor = new BlockEditor(window.wp, document.getElementById( 'editor' ));
+editor.init();
