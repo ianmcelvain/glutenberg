657893cc92ca27638693abce5c6074893cf7c791
Ian McElvain <ian.mcelvain@gmail.com>
2023-07-11 08:50:20 -0500

    Remove build files

--- public/js/pages/module.js
+++
@@ -1,8 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**************************************!*\
-  !*** ./resources/js/pages/module.js ***!
-  \**************************************/
-// TODO: import modules and write build config to properly serve it
-/******/ })()
-;
