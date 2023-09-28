0bc710cf7c160ecf676616c218579637e8a9fb05
Ian McElvain <ian.mcelvain@gmail.com>
2023-09-28 10:48:42 -0500

    Document react render logic

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -23,6 +23,13 @@ function onLoad(content, parser, rawHandler) {
   return rawHandler( { HTML: content } );
 }
 
+
+/**
+ * Not sure the best way of hooking into Laravel blade with React yet
+ * because I need to see more use-cases of React interacting with blade
+ * 
+ * This effectively is the same as @wordpress/element.render()
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <IsolatedBlockEditor
