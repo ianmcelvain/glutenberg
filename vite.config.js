b3fd62ad7b410a74ced107a3e7e16d0c6db8c54d
Ian McElvain <ian.mcelvain@gmail.com>
2023-09-27 20:41:27 -0500

    Clean vite config of comments

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-// import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.jsx',
-        ]),
+        laravel({
             refresh: true,
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+            ],
+        }),
         react(),
-        // vue({
-        //     template: {
-        //         transformAssetUrls: {
-        //             base: null,
-        //             includeAbsolute: false,
-        //         },
-        //     },
-        // }),
     ],
     define: {
         "process.env": {},
