033bad850229bb73476e27f6788470037791b403
Ian McElvain <ian.mcelvain@gmail.com>
2023-09-28 10:41:36 -0500

    Import editorSettings into app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,58 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import IsolatedBlockEditor, { EditorLoaded, ToolbarSlot, CollaborativeEditing } from '@automattic/isolated-block-editor';
+import editorSettings from '@constants/editor-settings';
 
 import './bootstrap';
 import '../sass/editor.scss';
 import '@automattic/isolated-block-editor/build-browser/core.css';
 import '@automattic/isolated-block-editor/build-browser/isolated-block-editor.css';
-
-const settings = {
-    iso: {
-      footer: true,
-      header: true,
-      allowApi: false,
-      linkMenu: [{
-        title: 'Cardinal Financial',
-        url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
-      }],
-      toolbar: {
-        inserter: true,
-        inspector: true,
-        navigation: true,
-        documentInspector: true,
-        toc: true,
-        undo: true,
-        selectorTool: true,
-      },
-      moreMenu: {
-        editor: true,
-        fullscreen: true,
-        preview: true,
-      },
-      sidebar: {
-        inserter: true,
-        inspector: true
-      }
-    },
-    editor: {
-      codeEditingEnabled: true,
-      hasUploadPermissions: true,
-      allowedMimeTypes: [],
-      allowedBlockTypes: [],
-      hasFixedToolbar: true,
-      template: null,
-      templateLock: null,
-      reusableBlocks: [],
-      styles: [],
-      defaultEditorStyles: [],
-      bodyPlaceholder: 'Glutenberg Module',
-      // __experimentalBlockPatterns: window.wp.__experimentalBlockPatterns ?? [],
-      // __experimentalBlockPatternCategories: window.wp.__experimentalBlockPatternCategories ?? []
-    },
-};
-
-const collabSettings = {};
 
 const saveContent = (html) => {
   console.log("🚀 ~ file: App.jsx:12 ~ saveContent ~ html:", html)
@@ -60,8 +14,7 @@ const saveContent = (html) => {
 
 function onLoad(content, parser, rawHandler) {
   // Does the content contain blocks?
-  if ( content.indexOf( '<!--' ) !== -1 ) {
-    // Parse the blocks
+  if ( content.indexOf( '<!--' ) !== -1 ) {    // Parse the blocks
     return parser( content );
   }
 
