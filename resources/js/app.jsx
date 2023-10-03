04903f8b1faa662bfa314c5e01c5fa90bb5f78a3
Ian McElvain <ian.mcelvain@gmail.com>
2023-10-03 12:30:03 -0500

    Apply wordpress render method

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-// import { StrictMode, createRoot } from '@wordpress/element';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client'
+import { StrictMode, createRoot } from '@wordpress/element';
 import Editor from '@components/react/editor';
 
 import '@/bootstrap';
+
 /**
  * Not sure the best way of hooking into Laravel blade with React yet
  * because I need to see more use-cases of React interacting with blade
  * 
 * This effectively is the same as @wordpress/element.render()
  */
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Editor />
-  </React.StrictMode>,
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Editor />
+//   </React.StrictMode>,
+// );
 
-// createRoot(document.getElementById('root'))
-//   .render(
-//     <StrictMode>
-//       <Editor />
-//     </StrictMode>
-//   );
+/**
+ * Wordpress method is identical to React on what it does under the hood.
+ * The benefit of using @wordpress/element is feeling confident on React
+ * version compatibility
+ */
+createRoot(document.getElementById('root'))
+  .render(
+    <StrictMode>
+      <Editor />
+    </StrictMode>
+  );
